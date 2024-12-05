7b7a54ef44dda0d27f2204c8a72beaca3c0238be
Alex Burrows <burrows.alexj@gmail.com>
2024-12-05 10:34:38 -0500

    Complete HostVanPrice component

--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -1,5 +1,11 @@
-
+import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPricing() {
-    return <h1>Host Van Pricing</h1>
+    const van = useOutletContext()
+
+    return (
+        <div className="host-van-price-container">
+            <p>${van.price}<span>/day</span></p>
+        </div>
+    )
 }
