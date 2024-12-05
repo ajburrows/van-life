791605f9c8a481a33ac9b90135eba41eac48c0a5
Alex Burrows <burrows.alexj@gmail.com>
2024-12-04 20:41:53 -0500

    Update server and initialize Host Van routes

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,14 +1,20 @@
-import { Link, Outlet  } from "react-router-dom"
+import { Link, NavLink, Routes, Outlet  } from "react-router-dom"
 import Dashboard from "../pages/Host/Dashboard"
 
 export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     return (
         <>
             <nav className="host-nav">
-                <Link to="host">Dashboard</Link>
-                <Link to="income">Income</Link>
-                <Link to="reviews">Reviews</Link>
+                <NavLink style={({ isActive }) => isActive ? activeStyles : null} to="host">Dashboard</NavLink>
+                <NavLink style={({ isActive }) => isActive ? activeStyles : null} to="income">Income</NavLink>
+                <NavLink style={({ isActive }) => isActive ? activeStyles : null} to="reviews">Reviews</NavLink>
+                <NavLink style={({ isActive }) => isActive ? activeStyles : null} to="vans">Vans</NavLink>
             </nav>
             <Outlet />
         </>
--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostLayout from './components/HostLayout';
+import HostVans from './pages/Host/HostVans';
 
 import "./server"
 
@@ -27,6 +28,8 @@ function App() {
             <Route path="host" element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<h1>vans/:id</h1>} />
           </Route>
         </Route>
       </Routes>
