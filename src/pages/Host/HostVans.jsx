47155f7dfdcf9f5d8f58e7cf59e43233a24348f2
Alex Burrows <burrows.alexj@gmail.com>
2024-12-04 21:07:10 -0500

    Pull van data from server and style the van cards

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,7 +1,33 @@
 import React from "react"
+import { useEffect, useState } from "react"
 
 export default function HostVans() {
+    const [vans, setVans] = useState([])
+
+    useEffect(() => {
+        fetch("/api/host/vans")
+            .then(resp => resp.json())
+            .then(data => setVans(data.vans))
+    } ,[])
+
+
+    const vansElements = vans.map((vanObj) => {
+        return (
+            <div className="host-van-card">
+                <img src={vanObj.imageUrl} />
+                <div>
+                    <h3>{vanObj.name}</h3>
+                    <p>${vanObj.price}/day</p>
+                </div>
+            </div>
+    )})
+
     return (
-        <h1>Host Vans List Page</h1>
+        <div className="host-van-main">
+            <div className="host-van-container">
+                <h1>Your listed vans</h1>
+                {vansElements}
+            </div>
+        </div>
     )
 }
