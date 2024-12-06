319164503a9e788cf02b5542d8c37b307c4b201f
Alex Burrows <burrows.alexj@gmail.com>
2024-12-05 22:08:55 -0500

    Add van type filters

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 
 export default function Vans() {
     const [vans, setVans] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const typeFilter = searchParams.get("type")
 
     useEffect(() => {
         fetch("api/vans")
@@ -13,9 +16,18 @@ export default function Vans() {
             })
     }, [])
 
-    const vanElements = vans.map(vanObj => (
+    function filterVans(vans){
+        if (typeFilter !== null){
+            let tmp = vans.filter(vanObj => vanObj.type.toLowerCase() === typeFilter.toLowerCase() ? vanObj : null)
+            return tmp 
+        }
+        return vans
+    }
+    const filteredVans = filterVans(vans)
+
+    const vanElements = filteredVans.map(vanObj => (
         <div key={vanObj.id} className="van-card">
-            <Link to={`/vans/${vanObj.id}`}>
+            <Link to={`${vanObj.id}`}>
                 <img src={vanObj.imageUrl} />
                 <div className="van-info">
                     <h3>{vanObj.name}</h3>
@@ -26,10 +38,26 @@ export default function Vans() {
         </div>
     ))
 
+    function getSelectedClassname(type) {
+        if (typeFilter == null || typeFilter.toLocaleLowerCase() !== type){
+            return ""
+        }
+        return " selected"
+    }
+
     return (
         <div className="van-main">
             <div className="van-list-container">
                 <h1>Explore our van options</h1>
+                    <div className="filter-container">
+                        <button onClick={() => setSearchParams({type: "simple"})} className={`van-type simple${getSelectedClassname("simple")}`} to="?type=simple">Simple</button>
+                        <button onClick={() => setSearchParams({type: "rugged"})} className={`van-type rugged${getSelectedClassname("rugged")}`} to="?type=rugged">Rugged</button>
+                        <button onClick={() => setSearchParams({type: "luxury"})} className={`van-type luxury${getSelectedClassname("luxury")}`} to="?type=luxury">Luxury</button>
+                        {typeFilter !== null
+                            ? <button onClick={() => setSearchParams({})} className="van-type clear-filters" to=".">Clear filter</button>
+                            : null
+                        }
+                    </div>
                 <div className="van-list">
                     {vanElements}
                 </div>
