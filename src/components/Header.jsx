a891b3e0648ab5c2bd0a067707699efcc2cafdbd
Alex Burrows <burrows.alexj@gmail.com>
2024-12-06 09:57:45 -0500

    Add login link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router-dom"
+import loginIconURL from "/src/assets/images/avatar-icon.png";
 
 export default function Header() {
     return(
@@ -8,7 +9,7 @@ export default function Header() {
           <NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/about">About</NavLink>
           <NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/vans">Vans</NavLink>
           <NavLink className={({isActive}) => isActive ? "active-link" : ""} to="/host">Host</NavLink>
-          <Link to="login" className="login-link"><img src="/src/assets/images/avatar-icon.png" className="login-icon"/></Link>
+          <Link to="login" className="login-link"><img src={loginIconURL} className="login-icon"/></Link>
         </nav>
       </header>
     )
